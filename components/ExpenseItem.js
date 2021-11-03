0c11729606d3f537f9cf3464b83b01fc680755eb
Jesse Orozco <orozcojesse0@gmail.com>
2021-11-02 20:26:26 -0400

    push work started

---
+++ components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>CarInsurance</h2>
+        <div>268.21</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
